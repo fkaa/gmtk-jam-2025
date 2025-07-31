f267b0aaeca4996f75dba34e6bcd37c26a1a67f0
Michael D'Agostino <mailmdag@gmail.com>
2025-07-31 12:19:30 -0500

    rework filth spawn to come from a dispenser at a fixed position on the belt

--- scenes/conveyor_belt/conveyor_belt.gd
+++ scenes/conveyor_belt/conveyor_belt.gd
@@ -14,20 +14,24 @@ const DIRTY_STUFF = [
 
 var _all_plates: Array[PlateOnBelt]
 
+var receiving_plate_idx = 5;
+
 
 func _ready() -> void:
 	for p in plates.get_children():
 		_all_plates.append(p as PlateOnBelt)
 
 func _process(delta: float) -> void:
+	var y_rotation_last = plates.rotation.y
 	plates.rotation.y += belt_speed * delta
-
-func _spawn_random_thing():
-	var random_plate = _all_plates.pick_random()
-	var random_thing = DIRTY_STUFF.pick_random().instantiate()
+	# If we've rotated 1/6 rotation, update receiving plate and spawn a filth
+	var rotation_increment_offset=(PI/3)/2
+	if floor((y_rotation_last+rotation_increment_offset) / (PI/3)) < floor((plates.rotation.y+rotation_increment_offset) / (PI/3)):
+		receiving_plate_idx -= 1
+		receiving_plate_idx %= _all_plates.size()
+		_spawn_random_thing()
 	
-	random_plate.add_item(random_thing)
-
-
-func _on_spawn_timer_timeout() -> void:
-	_spawn_random_thing()
+func _spawn_random_thing():
+	var receiving_plate = _all_plates[receiving_plate_idx]
+	var random_thing = DIRTY_STUFF.pick_random().instantiate()
+	receiving_plate.add_item(random_thing)
